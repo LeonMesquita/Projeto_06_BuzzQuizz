f75b5c1248abced2c214a4d1a82b6a8cd5d96685
LeonMesquita <leonmesquita16@gmail.com>
2022-04-20 19:29:09 -0300

    tela de informações básicas do quizz

---
+++ scripts/script.js
@@ -0,0 +1,103 @@
+const apiUrl = "https://mock-api.driven.com.br/api/v6/buzzquizz/";
+let uizzTitle;
+let quizzUrl;
+let questionsQuantity;
+let quizzLevels;
+let newQuizz;
+
+function setBasicInformations(){
+    basicInformations = document.querySelector(".basic-informations")
+    uizzTitle = basicInformations.querySelector(".title").value;
+    quizzUrl = basicInformations.querySelector(".url").value;
+    questionsQuantity = basicInformations.querySelector(".quantity").value;
+    quizzLevels = basicInformations.querySelector(".levels").value;
+
+    document.querySelector(".basic-informations").classList.toggle("escondido");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+formato do quizz:
+
+{
+	title: "Título do quizz",
+	image: "https://http.cat/411.jpg",
+	questions: [
+		{
+			title: "Título da pergunta 1",
+			color: "#123456",
+			answers: [
+				{
+					text: "Texto da resposta 1",
+					image: "https://http.cat/411.jpg",
+					isCorrectAnswer: true
+				},
+				{
+					text: "Texto da resposta 2",
+					image: "https://http.cat/412.jpg",
+					isCorrectAnswer: false
+				}
+			]
+		},
+		{
+			title: "Título da pergunta 2",
+			color: "#123456",
+			answers: [
+				{
+					text: "Texto da resposta 1",
+					image: "https://http.cat/411.jpg",
+					isCorrectAnswer: true
+				},
+				{
+					text: "Texto da resposta 2",
+					image: "https://http.cat/412.jpg",
+					isCorrectAnswer: false
+				}
+			]
+		},
+		{
+			title: "Título da pergunta 3",
+			color: "#123456",
+			answers: [
+				{
+					text: "Texto da resposta 1",
+					image: "https://http.cat/411.jpg",
+					isCorrectAnswer: true
+				},
+				{
+					text: "Texto da resposta 2",
+					image: "https://http.cat/412.jpg",
+					isCorrectAnswer: false
+				}
+			]
+		}
+	],
+	levels: [
+		{
+			title: "Título do nível 1",
+			image: "https://http.cat/411.jpg",
+			text: "Descrição do nível 1",
+			minValue: 0
+		},
+		{
+			title: "Título do nível 2",
+			image: "https://http.cat/412.jpg",
+			text: "Descrição do nível 2",
+			minValue: 50
+		}
+	]
+}
+
+
+*/
