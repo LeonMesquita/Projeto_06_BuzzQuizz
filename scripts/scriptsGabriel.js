e4e37cf012f46832af141cb5b9e8ae8ae28b048d
Gabriel <gabrielfalves2003@gmail.com>
2022-04-20 16:20:46 -0300

    'seus quizzes' página1

--- scripts/scriptsGabriel.js
+++ scripts/scriptsGabriel.js
@@ -1,16 +1,11 @@
 //Variáveis globais
 let arrQuizzes = [];
 
-function carregaQuizzes(){
-
+function carregaQuizzesTodos(){
     const promise = axios.get("https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes"); 
 
     promise.catch(windowReload);
     promise.then(renderizaQuizzes);
-}
-
-function windowReload(){
-    window.location.reload();
 }
 
 function renderizaQuizzes(response){ 
@@ -21,15 +16,14 @@ function renderizaQuizzes(response){
     for(let i = 0; i < arrQuizzes.length; i++){
         imprimeQuiz(arrQuizzes[i]);
     }
-
 }
 
 function imprimeQuiz(quiz){
 
-    const quizzes = document.querySelector(".quizzes");
+    const quizzes = document.querySelector(".quizzes")
 
     quizzes.innerHTML += `
-    <div class="quiz">
+    <div class="quiz" onclick="abrirQuiz()">
         <img src="${quiz.image}" alt="imagem quiz" />
         <p>${quiz.title}</p>
         <div class="gradiente"></div>
@@ -38,4 +32,39 @@ function imprimeQuiz(quiz){
 
 }
 
-carregaQuizzes();
+function carregaQuizzesUsuario(){
+    const promise = axios.get("https://mock-api.driven.com.br/api/v6/buzzquizz/quizzes"); 
+
+    promise.catch(windowReload);
+    promise.then(renderizaQuizzesUsuario);
+}
+
+function renderizaQuizzesUsuario(response){ 
+
+    arrQuizzes = response.data;
+    
+    document.querySelector(".quizzesUsuario").innerHTML = "";
+    for(let i = 0; i < arrQuizzes.length; i++){
+        imprimeQuizUsuario(arrQuizzes[i]);
+    }
+}
+
+function imprimeQuizUsuario(quiz){
+
+    const quizzes = document.querySelector(".quizzesUsuario");
+
+    quizzes.innerHTML += `
+    <div class="quiz margemDireita" onclick="abrirQuiz()">
+        <img src="${quiz.image}" alt="imagem quiz" />
+        <p>${quiz.title}</p>
+        <div class="gradiente"></div>
+    </div>
+    `
+
+}
+
+function windowReload(){
+    window.location.reload();
+}
+
+carregaQuizzesTodos();
